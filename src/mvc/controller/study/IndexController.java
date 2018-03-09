315dccf01470aa98ca28000215bb7f91b1c2def9
1103-5 <1103-5@1103-5-PC>
2018-03-09 16:02:49 +0900

    기본 프로젝트 완료

---
+++ src/mvc/controller/study/IndexController.java
@@ -0,0 +1,25 @@
+package mvc.controller.study;
+
+import org.omg.CORBA.Request;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import mvc.service.GreetService;
+
+@Controller
+@RequestMapping("/index")
+public class IndexController {
+	
+	@Autowired
+	GreetService greetService;
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String Alpha01Handler(Model model) {
+		model.addAttribute("ment", greetService.make());
+		return "index";
+	}
+
+}
---
+++ src/mvc/service/GreetService.java
@@ -0,0 +1,13 @@
+package mvc.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetService {
+	public String make() {
+		String[] ments = "어서오세요,WELCOME,いらっしゃいませ".split(",");
+				
+		return ments[(int)(Math.random()*ments.length)];
+	}
+
+}
